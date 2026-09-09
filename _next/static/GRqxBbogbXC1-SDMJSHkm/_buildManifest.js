0f4e2eea83d975cdf420fc45a4d100c60a7a25aa
hoangquanacca-hazel <hoangquanacca@gmail.com>
2026-09-09 13:35:16 +0700

    What-if interactive update

---
+++ _next/static/GRqxBbogbXC1-SDMJSHkm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-58bf98bda564513a.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
